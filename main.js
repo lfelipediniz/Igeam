1ef6431f5178319500f918876ce669d53208b246
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2024-03-28 14:10:28 -0300

    add typescript add tailwind

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const { app, BrowserWindow } = require('electron');
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: true,
+    },
   });
 
   win.loadURL('http://localhost:3000');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // 'media' or 'class'
+  content: [],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
-};
+}
 
